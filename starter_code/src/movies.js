0a478ad85d7ff4ccd39de4091002fbc3840730aa
Elsa Martinez <elsa.martinez.faucard@gmail.com>
2018-09-21 09:33:42 +0200

    Iteration 2 and 3 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,15 +4,46 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(array) {
+  var sum = array.reduce(function(acc, currentValue){
+    return acc + currentValue.rate;
+  },0);
+  return sum/array.length;
+
+}
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate(array){
+  var dramaMovies = array.filter(function(movie){
+    return  movie.genre.find(function(singleGenre){
+      return singleGenre === 'Drama';
+    });
+  });
+  if(!dramaMovies.length){
+    return undefined;
+  }
+  function precise(dramaMovies) {
+    return Number.parseFloat(dramaMovies).toPrecision(3);
+  };
+  return ratesAverage(dramaMovies);
+}
 
 // Order by time duration, in growing order
 
+function orderByDuration(array){
+  return array.sort(function(durationA,durationB){
+    return durationA-durationB;
+    if (durationA===durationB){
+      return movies.title.sort();
+    }
+  });
+}
 
 // How many movies did STEVEN SPIELBERG
 
+function howManyMovies(array) {
+  
+}
 
 // Order by title and print the first 20 titles
 
